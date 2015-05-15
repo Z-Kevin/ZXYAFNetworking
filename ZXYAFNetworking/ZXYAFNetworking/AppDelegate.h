d648ab535804de735192fcf4b5289050f71c4296
Z-Kevin <zhangxy0921@126.com>
2015-05-15 18:11:22 +0800

    创建新的工程 引入cocoapods

---
+++ ZXYAFNetworking/ZXYAFNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXYAFNetworking
+//
+//  Created by ZXY on 15/5/15.
+//  Copyright (c) 2015年 zxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
